baf0b7c18eb2712afec918c52ffc56958ed70f0d
Elifsi22 <113805051+Elifsi22@users.noreply.github.com>
2022-09-21 20:32:33 +0300

    conditionals

---
+++ conditionals/conditionals/src/Main.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        int sayi = 20;
+        if (sayi < 15)
+        {
+            System.out.println("sayı 15 den küçüktür.");
+        } else{
+            System.out.println("sayı 15 ten büyüktür");
+
+
+        }
+
+    }
+}
+
+
