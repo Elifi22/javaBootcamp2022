31f8850f9244ed3b64c2dac2cc1e8c540553c826
Elifsi22 <113805051+Elifsi22@users.noreply.github.com>
2022-09-21 19:14:08 +0300

    Variables

---
+++ variables/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int OgrenciSayisi = 12;
+        String  Mesaj = "Ögrenci sayım :";
+        System.out.println( Mesaj + OgrenciSayisi);
+        System.out.println( Mesaj + OgrenciSayisi );
+        System.out.println("Ögrenci sayım :" + OgrenciSayisi );
+        System.out.println("Ögrenci sayım :" + OgrenciSayisi);
+        System.out.println("Ögrenci sayım :" + OgrenciSayisi );
+
+    }
+
+
+}
