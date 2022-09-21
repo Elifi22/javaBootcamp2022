967b80d01f41f698f799e33d14177a973ad93103
Elifsi22 <113805051+Elifsi22@users.noreply.github.com>
2022-09-22 02:40:58 +0300

    while

---
+++ while/while/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 2; i < 10; i += 2) {
+            System.out.println(i);
+        }
+        System.out.println("For döngüsü bitti.");
+
+
+        int i = 2;
+
+        while (i < 10)
+        {
+            System.out.println(i);
+            i += 2;
+        }
+            System.out.println("while döngüsü bitti");
+
+        }
+
+
+    }
+
+
