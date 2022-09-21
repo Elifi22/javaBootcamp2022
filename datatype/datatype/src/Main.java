50cb327c0741381ec6fe2f713eb6d232e1555b6b
Elifsi22 <113805051+Elifsi22@users.noreply.github.com>
2022-09-21 19:57:56 +0300

    datatypes

---
+++ datatype/datatype/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        char Karakter='A';
+        boolean dogruMu=false;
+        int sayi= 22;
+        double sayi2=12.5;
+        sayi=-129;
+        String yemek="Lahmacun";
+    }
+}
