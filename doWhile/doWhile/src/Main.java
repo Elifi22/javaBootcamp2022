5c4eae25d04652d70a120521979c8439152dcc81
Elifsi22 <113805051+Elifsi22@users.noreply.github.com>
2022-09-22 15:06:14 +0300

    doWhile

---
+++ doWhile/doWhile/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int i =0;
+
+        do {
+         System.out.println(i);
+         i+=2;
+
+        }
+        while (i<10);
+        System.out.println("do while döngüsü btii.");   }
+}
