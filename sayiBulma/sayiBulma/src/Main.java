3a30f61ab0cd34bb2c57b09e332b5e03e2624081
Elifsi22 <113805051+Elifsi22@users.noreply.github.com>
2022-09-28 00:11:01 +0300

    sayiBulma

---
+++ sayiBulma/sayiBulma/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] sayilar = new int[] { 1, 2, 3, 4, 5, 6 };
+        int aranacak = 6;
+        boolean varMı=false;
+
+        for (int i = 0; i < sayilar.length; i++) {
+            if (aranacak == sayilar[i]) {
+            }
+
+
+
+
+
+
+
+        } varMı=true;
+        if (varMı){
+        System.out.println("Sayı mevcuttur");
+    }
+        else {
+            System.out.println("sayı mevcut değil.");
+        }
+}
+}
