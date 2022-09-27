d22f2155a64c381ea38b4d3db4ad1702eb04c458
Elifsi22 <113805051+Elifsi22@users.noreply.github.com>
2022-09-27 20:20:52 +0300

    sesliHarfler

---
+++ sesliHarfler/sesliHarfler/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        char harf = 'E';
+        switch (harf) {
+            case 'I':
+            case 'o':
+            case 'u':
+            case 'A':
+
+                System.out.println("kalın sesli  harflidir");
+              break;
+            default:
+                System.out.println("ince sesli harftir ");
+
+
+
+
+
+        }
+    }
+}
